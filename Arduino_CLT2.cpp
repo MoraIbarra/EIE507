694527cb68c394af3a266a23a658148a10f8cc59
MoraIbarrra <129218779+MoraIbarra@users.noreply.github.com>
2023-06-29 23:42:04 -0400

    Update Arduino_CLT2.cpp

--- Arduino_CLT2.cpp
+++ Arduino_CLT2.cpp
@@ -11,10 +11,9 @@
 
 OneWire oneWire[NUM_SENSORS];
 DallasTemperature sensors[NUM_SENSORS];
-// Configuration of the NRF24 module
-RF24 radio(9, 10);  // CE and CSN pins, respectively
 
-// Structure to store sensor information
+RF24 radio(8, 10);
+
 struct SensorData {
   int sensorNumber;
   float temperature;
@@ -23,7 +22,6 @@ struct SensorData {
 void setup() {
   Serial.begin(9600);
 
-  // Inicializar la comunicación con cada sensor
   oneWire[0] = OneWire(SENSOR_PIN_1);
   sensors[0] = DallasTemperature(&oneWire[0]);
 
@@ -33,35 +31,26 @@ void setup() {
   oneWire[2] = OneWire(SENSOR_PIN_3);
   sensors[2] = DallasTemperature(&oneWire[2]);
 
-  // Iniciar la comunicación con los sensores
   for (int i = 0; i < NUM_SENSORS; i++) {
     sensors[i].begin();
   }
 
-  // Initialize the NRF24 module
   radio.begin();
   radio.setPALevel(RF24_PA_HIGH);
 
-  // Configure the reading pipe for channel 1
-  radio.openReadingPipe(1, 0xF0F0F0F0E1LL);  // Receiver address (Arduino receiver)
-  // Set the transmission speed to 250 kbps
+  radio.openReadingPipe(0xF0F0F0F0C3LL);
+
   radio.setDataRate(RF24_250KBPS);
 
-  // Set the transmission power to a high level
   radio.setPALevel(RF24_PA_HIGH);
 
-  // Start listening on channel 1
   radio.startListening();
 }
 
 void loop() {
-  // Check if there is data available to receive on channel 1
   if (radio.available()) {
-    // Read the sensor information
     SensorData data;
     radio.read(&data, sizeof(data));
-
-    // Print the received information from channel 1
     Serial.print("Sensor ");
     Serial.print(data.sensorNumber);
     Serial.print(" - Temperature: ");
